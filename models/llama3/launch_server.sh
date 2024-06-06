330b9e09966c96095a142745f6f846d80b84511f
Marshall Wang <marshallxkwang@gmail.com>
2024-06-06 14:35:45 -0400

    Update poetry lock, add mistral models, update default env to use singularity container, but multi-node hosting broken

--- models/llama3/launch_server.sh
+++ models/llama3/launch_server.sh
@@ -11,6 +11,7 @@ export VLLM_BASE_URL_FILENAME="${MODEL_DIR}/.vLLM_${MODEL_NAME}-${MODEL_VARIANT}
  
 # Variables specific to your working environment, below are examples for the Vector cluster
 export VENV_BASE=singularity
+export VLLM_NCCL_SO_PATH=/vec-inf/nccl/libnccl.so.2.18.1
 export VLLM_MODEL_WEIGHTS=/model-weights/${MODEL_NAME}-${MODEL_VARIANT}
 export LD_LIBRARY_PATH="/scratch/ssd001/pkgs/cudnn-11.7-v8.5.0.96/lib/:/scratch/ssd001/pkgs/cuda-11.7/targets/x86_64-linux/lib/"
 
