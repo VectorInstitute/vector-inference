c4dbed03d798e46079c1ffc2bddd2f86daa4793f
Marshall Wang <marshallxkwang@gmail.com>
2024-06-13 13:18:26 -0400

    Update examplesto include VLM completions, add profiling scripts

--- examples/logits/logits.py
+++ examples/logits/logits.py
@@ -1,12 +1,12 @@
 from openai import OpenAI
 
-# The url is located in the .vllm_model-variant_url file in the corresponding model directory.
+# The url is located in the .vLLM_model-variant_url file in the corresponding model directory.
 client = OpenAI(base_url="http://gpuXXX:XXXXX/v1", api_key="EMPTY")
 
 completion = client.completions.create(
-    model="/model-weights/Llama-2-7b-hf",
+    model="/model-weights/Meta-Llama-3-8B",
     prompt="Where is the capital of Canada?",
-    max_tokens=20,
+    max_tokens=1,
     logprobs=32000 # Set to model vocab size to get logits
 )
 
